LineageOS/android_vendor_lineage

config: Exclude product_manifest.xml from enforced existing packages
Not all build targets have this target modules

Change-Id: I01051bb0f6e285ca64bfbfbbaa70bfab796533eb

config/common.mk
@@ -101,7 +101,7 @@ PRODUCT_MINIMIZE_JAVA_DEBUG_INFO := true
 PRODUCT_RESTRICT_VENDOR_FILES := false
 
 # Require all requested packages to exist
-$(call enforce-product-packages-exist-internal,$(wildcard device/*/$(LINEAGE_BUILD)/$(TARGET_PRODUCT).mk),)
+$(call enforce-product-packages-exist-internal,$(wildcard device/*/$(LINEAGE_BUILD)/$(TARGET_PRODUCT).mk),product_manifest.xml)
 
 # Bootanimation
 TARGET_SCREEN_WIDTH ?= 1080

theorchidproject/vendor_aospGoogle

config/common.mk
@@ -2,7 +2,7 @@
 $(call inherit-product, vendor/aosp/config/branding.mk)
 
 # Require all requested packages to exist
-$(call enforce-product-packages-exist-internal,$(wildcard device/*/$(CUSTOM_BUILD)/$(TARGET_PRODUCT).mk),)
+$(call enforce-product-packages-exist-internal,$(wildcard device/*/$(CUSTOM_BUILD)/$(TARGET_PRODUCT).mk),product_manifest.xml)
 
 PRODUCT_BRAND ?= PixelExperience
 