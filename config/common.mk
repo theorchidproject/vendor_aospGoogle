1b86e213f6900ba88d677edd8bf3fdbdef2268b9
theorchidproject <84352313+theorchidproject@users.noreply.github.com>
2023-01-24 21:03:44 +0000

    OrchidOs Application Update : Add Lawnchair Launcher
    Added Lawnchair Launcher overriding system launcher
    added permission s
    added to hidden applications
    for future inbuilt applications and system settings applications
    https://github.com/LawnchairLauncher

--- config/common.mk
+++ config/common.mk
@@ -1,6 +1,7 @@
 # Branding
 $(call inherit-product, vendor/aosp/config/branding.mk)
 $(call inherit-product-if-exists, vendor/bromite_system_inline/bromite.mk)
+$(call inherit-product-if-exists, vendor/lawnchair/lawnchair.mk)
 
 
 PRODUCT_BRAND ?= PixelExperience
---
+++ lawnchair/lawnchair.mk
@@ -0,0 +1,10 @@
+# Lawnchair
+PRODUCT_SOONG_NAMESPACES += \
+    vendor/lineage/orchidoscore/lawnchair
+
+PRODUCT_PACKAGES += \
+    Lawnchair \
+    LawnchairFrameworksOverlay \
+    LawnchairOverlay \
+    lawnchair-hiddenapi-package-whitelist \
+    privapp-permissions-lawnchair
